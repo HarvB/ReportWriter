65d1537420f4e6a4394b9a4ea808a856fc45eae5
HarvB <bird.harvey@outlook.com>
2020-09-26 11:14:52 +0100

    Created DataReader

--- Collater.cs
+++ Collater.cs
@@ -31,5 +31,9 @@ namespace ReportWriter
                 }
             }
         }
+        public void WriteLineRow()
+        {
+            
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,21 +6,22 @@ namespace ReportWriter
     {
         static void Main(string[] args)
         {
-            var lineRow1 = new LineRow();
-            var lineRow2 = new LineRow();
-            lineRow1.LineText = "My New Doc";
-            lineRow2.LineText = "Another Line";
-            var section = new Section();
-            section.SectionTitle = "Turnips";
-            section.LineRows.Add(lineRow1);
-            section.LineRows.Add(lineRow2);
             var collater = new Collater(@"D:\Harvey\OneDrive\IT\MyDoc.docx");
-            collater.WriteSection(section);
+
             var csv = new DataReaderCsv();
             foreach(var t in csv.ReadCSV(@".\Data\FirstData.csv"))
             {
-                Console.WriteLine($"{t.Title}: {t.Words}");
+                var section = new Section();
+                section.SectionTitle = t.Title;
+                var lineRow1 = new LineRow();
+                lineRow1.LineText = t.Words;
+                var lineRow2 = new LineRow();
+                lineRow2.LineText = t.More_Words;
+                section.LineRows.Add(lineRow1);
+                section.LineRows.Add(lineRow2);
+                collater.WriteSection(section);
             }
+                            
         }
     }
 }
